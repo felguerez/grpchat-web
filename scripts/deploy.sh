de59a824dc5a303d8c0b83bbeae30b30160db997
Antonio Felguerez <antoniof@spotify.com>
2023-10-29 15:14:25 -0400

    Another attempt

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,9 @@
 CLUSTER_NAME="grpchat-grpc-cluster"
 SERVICE_NAME="grpchat-web"
 ECR_REPO="413025517373.dkr.ecr.us-east-1.amazonaws.com/grpchat-web"
-SCRIPT_DIR="$(dirname "$0")"  # This gets the directory where the script is located
+PROJECT_ROOT_DIR="$(dirname "$0")/.."  # This gets the parent directory of the script location
+
+echo "Project directory is $PROJECT_ROOT_DIR"
 
 # login
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPO
@@ -12,7 +14,7 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 cd "$SCRIPT_DIR/.."
 
 # Build Docker image
-docker build -t $SERVICE_NAME .
+docker build -t $SERVICE_NAME "$PROJECT_ROOT_DIR"
 
 # Tag Docker image for Amazon ECR
 docker tag $SERVICE_NAME:latest $ECR_REPO:latest
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { NewConversationForm } from "@/app/components/NewConversationForm";
 import { cookies, headers } from "next/headers";
-import ChatRoom from "@/app/components/ChatRoom";
 
 export default function Home() {
   const cookieStore = cookies();
   const sessionId = cookieStore.get("session_id");
-  console.log("sessionId:", sessionId);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>welcome to cover chat</h1>
