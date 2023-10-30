cac1f8412b2c2f39031fd9875e773b0c2fd3a8ba
Antonio Felguerez <antoniof@spotify.com>
2023-10-30 00:35:54 -0400

    Attempt to fix invalid url

--- src/app/api/authenticated/route.ts
+++ src/app/api/authenticated/route.ts
@@ -1,17 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const sessionId = request.headers.get("sessionId");
+  const sessionId = request.headers.get('sessionId');
+
+  // Generate an absolute URL for redirect
+  const host = request.headers.get('host');
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+  const rootUrl = `${protocol}://${host}`;
+
   if (sessionId) {
     // If sessionId exists, set it as a cookie and redirect to root
-    const response = NextResponse.redirect('/');
+    const response = NextResponse.rewrite('/', {status: 302});
     response.cookies.set('sessionId', sessionId, {
       path: '/',
-      httpOnly: true
+      // other cookie options like secure, httpOnly, etc.
     });
+    response.headers.set('Location', `${rootUrl}/`);
     return response;
   } else {
     // If sessionId doesn't exist, redirect to root with an error message
-    return NextResponse.redirect('/?error=NoSessionId');
+    return NextResponse.redirect(`${rootUrl}/?error=NoSessionId`);
   }
 }
