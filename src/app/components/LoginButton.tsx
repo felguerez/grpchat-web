cd1b9eea9528e897669120ec1d0d73cfd494937e
Antonio Felguerez <antoniof@spotify.com>
2023-11-16 08:26:15 -0500

    Remove noisy log

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 
 export function LoginButton({ url }: {  url: string; }) {
-  // const url = `${process.env.API_URL}/login`;
-  console.log('Object.keys(process.env):', Object.keys(process.env));
   console.log('url:', url);
   function onLogin() {
     window.location.assign(url);
